1a5cee7fa06a377b7de74bd4884ca3c4947d5b9a
Token-05 <take523oliver@gmail.com>
2021-03-30 22:49:44 +0900

    Add button

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class Board extends React.Component {
             board: 6,//board × board盤面
             suqares: {
                 id: Array(36).fill(null),
-                status: Array(36).fill(false),//未接触:false 接触済:true
+                // status: Array(36).fill(false),//未接触:false 接触済:true
                 contents: Array(36).fill(false),//何もない:false 爆弾あり:true
             }
         };
@@ -27,26 +27,26 @@ class Board extends React.Component {
 
     eventhandle(place) {
         const board = this.state.board;
-
-        const newsuqaresState = Object.assign({}, this.state.suqares);
-        newsuqaresState.contents[place] = true;
-        this.setState({ suqares: newsuqaresState });
         document.getElementById(place).style.display = 'none';
         document.getElementById(place+board*board).style.display = 'inline-block';
+    }
+
+    bombInst() {
+        
     }
 
     render() {
         const suqaresOne = this.state.suqares;
         const board = this.state.board;
-
         for(let i = 0; i < board; i++){
             this.list.push(<div className="board-row" />);
             for(let j = 0; j < board; j++){
                 this.list.push(<div className="square tapped" id={i*board+j+board*board} value={suqaresOne.contents[i*board+j]}>2</div>);
                 this.list.push(<button className="square tap" id={i*board+j} value={suqaresOne.contents[i*board+j]} 
-                onClick={() => this.eventhandle(i*board+j)}>1</button>);
+                onClick={() => this.eventhandle(i*board+j)}>?</button>);
             }
         }
+
         return (
             <div>
                 <p>{this.state.board} × {this.state.board}盤面です</p>
